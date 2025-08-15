1603e34ed859948fc1f7bae8d85e9f1318905328
pritamsarkar5058-netizen <pritamsarkar5058@gmail.com>
2025-08-15 22:26:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "DML-MD~FvkRTBBA#leRv2SOSXxswv5DhlHk5vpmRhX_S6OoL27Fcjn-hzHw",
+SESSION_ID: process.env.SESSION_ID || "DML-MD~0usTEbha#mdWHUNWVniYzui5zL_9u-8SdNRmyFxDRTC7eXZlGvlg",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
